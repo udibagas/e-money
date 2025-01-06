5e8e3a125da16de228e5a8a6c1e92d00947edd95
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-06 09:45:11 +0700

    make oop

--- card.js
+++ card.js
@@ -1,3 +1,5 @@
+const { validityFlags, cardTypes } = require("./constants");
+
 class Card {
   constructor(
     type = "",
@@ -13,6 +15,35 @@ class Card {
     this.balance = balance;
   }
 
+  static create(data) {
+    // 06 00 00 00 0F 01 5E B5 03 93 60 88 50 00 00 00 14 00 00 4D 7F
+    // 06 = Card Type = Jakcard DKI Card
+    // 00 00 00 0F 01 5E B5 = Card UID (0F015EB5)
+    // 03 = Card Data validity flag = Both card number and card balance is valid
+    // 93 60 88 50 00 00 00 14 = Card Number (9360 8850 0000 0014)
+    // 00 00 4D 7F = Card Balance (Rp. 19.839)
+
+    // 060000000F015EB503936088500000001400004D7F
+
+    // 02 0015020000008f441e1d 03 6032984031044294 00006f54 8e e money
+    // 02 00150500000047bb9fc2 03 0145202200110508 00001194 6d flazz
+    // 02 0015030456d5e2db7580 01 6013500467325578 00000000 03 bri
+
+    const cardType = data.slice(0, 2);
+    const cardUID = data.slice(2, 22);
+    const cardDataValidityFlag = data.slice(22, 24);
+    const cardNumber = data.slice(24, 40);
+    const cardBalance = data.slice(40, 48);
+
+    return new Card(
+      cardType,
+      cardUID,
+      cardDataValidityFlag,
+      cardNumber,
+      parseInt(cardBalance, 16)
+    );
+  }
+
   reset() {
     this.type = "";
     this.uid = "";
@@ -20,6 +51,33 @@ class Card {
     this.number = "";
     this.balance = 0;
   }
+
+  get typeDescription() {
+    return cardTypes[this.type];
+  }
+
+  get insufficientBalance() {
+    return this.dataValidityFlag === "01";
+  }
+
+  get invalidCardNumber() {
+    return this.dataValidityFlag === "00" || this.dataValidityFlag === "02";
+  }
+
+  get isValid() {
+    return this.dataValidityFlag === "03";
+  }
+
+  get dataValidityFlagDescription() {
+    return validityFlags[this.dataValidityFlag];
+  }
+
+  get balanceFormatted() {
+    return this.balance.toLocaleString("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    });
+  }
 }
 
-module.exports = new Card();
+module.exports = Card;
--- gateIn.js
+++ gateIn.js
@@ -1,41 +1,8 @@
 require("dotenv").config();
 const { default: axios } = require("axios");
 const { SerialPort } = require("serialport");
-
+const Card = require("./card");
 const { cardTypes, VALID, validityFlags } = require("./constants");
-
-function parseData(data) {
-  // 06 00 00 00 0F 01 5E B5 03 93 60 88 50 00 00 00 14 00 00 4D 7F
-  // 06 = Card Type = Jakcard DKI Card
-  // 00 00 00 0F 01 5E B5 = Card UID (0F015EB5)
-  // 03 = Card Data validity flag = Both card number and card balance is valid
-  // 93 60 88 50 00 00 00 14 = Card Number (9360 8850 0000 0014)
-  // 00 00 4D 7F = Card Balance (Rp. 19.839)
-
-  // 060000000F015EB503936088500000001400004D7F
-
-  // 02 0015020000008f441e1d 03 6032984031044294 00006f54 8e e money
-  // 02 00150500000047bb9fc2 03 0145202200110508 00001194 6d flazz
-  // 02 0015030456d5e2db7580 01 6013500467325578 00000000 03 bri
-
-  const cardType = data.slice(0, 2);
-  const cardUID = data.slice(2, 22);
-  const cardDataValidityFlag = data.slice(22, 24);
-  const cardNumber = data.slice(24, 40);
-  const cardBalance = data.slice(40, 48);
-
-  return {
-    cardType: cardTypes[cardType],
-    cardUID,
-    cardDataValidityFlag,
-    cardDataValidityFlagText: validityFlags[cardDataValidityFlag],
-    cardNumber,
-    cardBalance: parseInt(cardBalance, 16),
-  };
-}
-
-// console.log(parseData("060000000F015EB503936088500000001400004D7F"));
-// console.log(parseData("020015020000008f441e1d03603298403104429400006f548e"));
 
 const port = new SerialPort({
   path: process.env.SERIAL_PORT_IN || "/dev/ttyUSB0",
@@ -64,21 +31,10 @@ port.on("error", (error) => {
 port.on("data", (data) => {
   data = data.toString("hex");
   console.log("Data received: " + data);
-
-  const {
-    cardType,
-    cardUID,
-    cardDataValidityFlag,
-    cardDataValidityFlagText,
-    cardNumber,
-    cardBalance,
-  } = parseData(data);
-
-  console.log("Card Type: " + cardType);
-  console.log("Card UID: " + cardUID);
-  console.log(
-    `Card Data Validity Flag: [${cardDataValidityFlag}] ${cardDataValidityFlagText}`
-  );
-  console.log("Card Number: " + cardNumber);
-  console.log("Card Balance: Rp. " + cardBalance.toLocaleString());
+  const card = Card.create(data);
+  console.log("Card type:", card.typeDescription);
+  console.log("Card UID:", card.uid);
+  console.log("Card data validity flag:", validityFlags[card.dataValidityFlag]);
+  console.log("Card number:", card.number);
+  console.log("Card balance:", card.balanceFormatted);
 });
