d8ae82ab743bc97033e32fae89d1c3e80bc3a812
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-08 21:08:35 +0700

    test buzz

--- gateOut.js
+++ gateOut.js
@@ -5,3 +5,4 @@ const { SERIAL_PORT_OUT } = process.env;
 if (!SERIAL_PORT_OUT) throw new Error("SERIAL_PORT_OUT is required");
 
 const device = new Device(SERIAL_PORT_OUT);
+setTimeout(() => device.buzz(), 5000);
